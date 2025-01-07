57aec83bcc854d300c4cf7f6afd54b532a9a1251
aliiftikhar1 <20011519-054@uog.edu.pk>
2025-01-07 15:03:42 +0500

    done

--- src/app/components/PartenerLogos.js
+++ src/app/components/PartenerLogos.js
@@ -7,16 +7,20 @@ const Partners = () => {
     { name: "Hotel Chain Partner", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTZdJHXpPo3Sp-HM-X4LQ1NVyIaGyBM782Ng&s" },
     { name: "Tourism Board", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl61HKPLtJJU74djdcJDtlQyv4n8SuufbuJRf_5uV8ccjMCPWho68gPWVW7E-DyBCxqnQ&usqp=CAU" },
     { name: "Travel Partner 4", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSBqpdmgnpStGPirhhpKys23VwelJhTC5q9fCgG3_tIOgmdebRP0K2Ym8AQmdrbfwM_U&usqp=CAU" },
+    { name: "Airline Partner 1", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmk4w4MeIg25dFIpSqW3O6paa6a6IlQ7zQw&s" },
+    { name: "Hotel Chain Partner", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTZdJHXpPo3Sp-HM-X4LQ1NVyIaGyBM782Ng&s" },
+    { name: "Tourism Board", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl61HKPLtJJU74djdcJDtlQyv4n8SuufbuJRf_5uV8ccjMCPWho68gPWVW7E-DyBCxqnQ&usqp=CAU" },
+    { name: "Travel Partner 4", logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdSBqpdmgnpStGPirhhpKys23VwelJhTC5q9fCgG3_tIOgmdebRP0K2Ym8AQmdrbfwM_U&usqp=CAU" },
   ];
 
   return (
     <section id="partners" className="bg-white py-16">
       <div className="md:w-[87vw] mx-auto text-center flex flex-col md:grid grid-cols-6 gap-2">
         <div className="flex flex-col col-span-2 justify-start md:items-start py-10  ">
-          <h2 className="text-3xl font-bold mb-4">Top Popular Airline In Pakistan</h2>
+          <h2 className="text-3xl font-bold mb-4 md:max-w-sm md:text-left">Top Popular Airline In Pakistan</h2>
           <p className=""> Our airline partners are the best in the industry.</p>
         </div>
-        <div className="col-span-4 flex flex-wrap justify-between gap-1 md:px-0 px-4">
+        <div className="grid grid-cols-6 col-span-4 gap-2">
           <img src="https://logowik.com/content/uploads/images/pakistan-international-airlines4661.logowik.com.webp" className="size-32"></img>
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2jLtQHwB50n3vE3j-OLhAwARx-Adudntcbg&s" className="size-32"></img>
           <img src="https://logowik.com/content/uploads/images/pakistan-international-airlines4661.logowik.com.webp" className="size-32"></img>
@@ -30,12 +34,12 @@ const Partners = () => {
       </div>
       <div className="md:w-[87vw] mx-auto text-center flex flex-col  md:grid grid-cols-6 gap-2">
         <div className="flex flex-col col-span-2 justify-start md:items-start md:py-0 py-10 ">
-          <h2 className="text-3xl font-bold mb-4">Our Trusted Parteners</h2>
+          <h2 className="text-3xl font-bold mb-4 md:max-w-sm md:text-left">Our Trusted Parteners</h2>
           <p> Our partners are the best in the industry.</p>
         </div>
-        <div className="col-span-4 flex flex-wrap justify-between gap-1 md:px-0 px-4">
+        <div className="grid grid-cols-6 col-span-4 gap-2">
           {partners.map((partner, index) => (
-            <div key={index} className="flex justify-center size-32">
+            <div key={index} className="flex justify-center size-32 border">
               <img
                 src={partner.logo}
                 alt={partner.name}
